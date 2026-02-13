9c78c19d41698b3afcd6c5fcf40ff638bbe4f6b5
Aisha Zarrah <aishazarrah@gmail.com>
2026-02-13 20:33:51 +0700

    feat: differentiate user and admin ui in rooms page

--- src/pages/RoomsPage.tsx
+++ src/pages/RoomsPage.tsx
@@ -3,8 +3,11 @@ import Layout from "../components/Layout";
 import { getRooms, deleteRoom, createRoom, updateRoom } from "../api/rooms";
 import toast from "react-hot-toast";
 import { formatToWIB } from "../utils/date";
+import { getCurrentUser } from "../utils/auth";
 
 export default function RoomsPage() {
+  const user = getCurrentUser();
+  const isAdmin = user?.role === "Admin";
   const [search, setSearch] = useState("");
   const [rooms, setRooms] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
@@ -227,27 +230,29 @@ export default function RoomsPage() {
       <div className="flex items-center justify-between mb-4">
         <p className="text-3xl font-extrabold">Rooms</p>
 
-        <div className="flex gap-2 text-center">
-          <button
-            onClick={() => setOpenCreate(true)}
-            className="flex items-center gap-2 rounded-lg bg-[#FEF3C7]! hover:bg-[#fde68a] font-semibold"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-plus-lg"
-              viewBox="0 0 16 16"
+        {isAdmin && (
+          <div className="flex gap-2 text-center">
+            <button
+              onClick={() => setOpenCreate(true)}
+              className="flex items-center gap-2 rounded-lg bg-[#FEF3C7]! hover:bg-[#fde68a] font-semibold"
             >
-              <path
-                fill-rule="evenodd"
-                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
-              />
-            </svg>{" "}
-            <p className="text-sm">Add Room</p>
-          </button>
-        </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-plus-lg"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
+                />
+              </svg>{" "}
+              <p className="text-sm">Add Room</p>
+            </button>
+          </div>
+        )}
       </div>
 
       {/* TABLE */}
@@ -261,7 +266,7 @@ export default function RoomsPage() {
                 <th className="px-4 py-2 font-bold">Room Code</th>
                 <th className="px-4 py-2 font-bold">Building</th>
                 <th className="px-4 py-2 font-bold">Capacity</th>
-                <th className="px-4 py-2 font-bold">Action</th>
+                {isAdmin && <th className="px-4 py-2 font-bold">Action</th>}
               </tr>
             </thead>
             <tbody className="text-black font-bold text-center">
@@ -278,75 +283,77 @@ export default function RoomsPage() {
                   <td className="px-4 py-1">{r.name}</td>
                   <td className="px-4 py-1">{r.building}</td>
                   <td className="px-4 py-1">{r.capacity}</td>
-                  <td className="px-4 py-1 text-center">
-                    <div className="flex justify-center gap-3">
-                      {/* BUTTON VIEW */}
-                      <button
-                        onClick={() => {
-                          setViewRoom(r);
-                          setOpenView(true);
-                        }}
-                        className="hover:text-blue-500"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-eye"
-                          viewBox="0 0 16 16"
+                  {isAdmin && (
+                    <td className="px-4 py-1 text-center">
+                      <div className="flex justify-center gap-3">
+                        {/* BUTTON VIEW */}
+                        <button
+                          onClick={() => {
+                            setViewRoom(r);
+                            setOpenView(true);
+                          }}
+                          className="hover:text-blue-500"
                         >
-                          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
-                        </svg>
-                      </button>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-eye"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                          </svg>
+                        </button>
 
-                      {/* BUTTON EDIT */}
-                      <button
-                        onClick={() => {
-                          setEditingRoom(r);
-                          setEditForm({
-                            name: r.name,
-                            building: r.building,
-                            capacity: r.capacity,
-                          });
-                          setOpenEdit(true);
-                        }}
-                        className="hover:text-green-500"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-pencil"
-                          viewBox="0 0 16 16"
+                        {/* BUTTON EDIT */}
+                        <button
+                          onClick={() => {
+                            setEditingRoom(r);
+                            setEditForm({
+                              name: r.name,
+                              building: r.building,
+                              capacity: r.capacity,
+                            });
+                            setOpenEdit(true);
+                          }}
+                          className="hover:text-green-500"
                         >
-                          <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-                        </svg>
-                      </button>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-pencil"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                          </svg>
+                        </button>
 
-                      {/* BUTTON DELETE */}
-                      <button
-                        onClick={() => {
-                          setDeleteRoomId(r.id);
-                          setOpenDelete(true);
-                        }}
-                        className="hover:text-red-500"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-trash3"
-                          viewBox="0 0 16 16"
+                        {/* BUTTON DELETE */}
+                        <button
+                          onClick={() => {
+                            setDeleteRoomId(r.id);
+                            setOpenDelete(true);
+                          }}
+                          className="hover:text-red-500"
                         >
-                          <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                        </svg>
-                      </button>
-                    </div>
-                  </td>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-trash3"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                          </svg>
+                        </button>
+                      </div>
+                    </td>
+                  )}
                 </tr>
               ))}
             </tbody>
@@ -430,7 +437,7 @@ export default function RoomsPage() {
       )}
 
       {/* MODAL CREATE */}
-      {openCreate && (
+      {isAdmin && openCreate && (
         <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
           <div className="bg-white rounded-xl w-full max-w-md p-6 shadow-lg">
             <h2 className="text-xl font-bold mb-4 text-center">Add Room</h2>
@@ -494,7 +501,7 @@ export default function RoomsPage() {
       )}
 
       {/* MODAL EDIT */}
-      {openEdit && editingRoom && (
+      {isAdmin && openEdit && editingRoom && (
         <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
           <div className="bg-white rounded-xl w-full max-w-md p-6 shadow-lg">
             <h2 className="text-xl font-bold mb-4 text-center">Edit Room</h2>
@@ -563,7 +570,7 @@ export default function RoomsPage() {
       )}
 
       {/* MODAL VIEW */}
-      {openView && viewRoom && (
+      {isAdmin && openView && viewRoom && (
         <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
           <div className="bg-white rounded-xl w-full max-w-md p-6 shadow-lg">
             <h2 className="text-xl font-bold mb-4 text-center">Room Detail</h2>
@@ -623,10 +630,13 @@ export default function RoomsPage() {
       {openDelete && (
         <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
           <div className="bg-white rounded-xl w-full max-w-sm p-6 shadow-lg">
-            <h2 className="text-lg font-bold mb-2 text-red-600 text-center">Delete Room</h2>
+            <h2 className="text-lg font-bold mb-2 text-red-600 text-center">
+              Delete Room
+            </h2>
 
             <p className="text-sm text-gray-600 mb-5">
-              Are you sure you want to delete this room? You won't be able to undo this action.
+              Are you sure you want to delete this room? You won't be able to
+              undo this action.
             </p>
 
             <div className="flex justify-end gap-2">
